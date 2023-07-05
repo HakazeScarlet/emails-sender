2f19b3630226de2c532fb7061f32ebf319a75754
hakazescarlet <frost.scarlet.163@gmail.com>
2023-07-05 22:12:08 +0300

    extracted session configuration in separate class, extracted email and password in environment variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
 import parser.Recipient;
 import parser.RecipientCsvParser;
 
-import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -16,30 +14,13 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) throws MessagingException, URISyntaxException, IOException {
-        Properties prop = new Properties();
-        prop.put("mail.smtp.starttls", true);
-        prop.put("mail.smtp.starttls.enable", true);
-        prop.put("mail.smtp.starttls.required", true);
-        prop.put("mail.smtp.auth", true);
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", 587);
-        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-        prop.put("mail.smtp.EnableSSL.enable", true);
-        prop.put("mail.smtp.ssl.protocols", "TLSv1.2");
-        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-
-
-        Session session = Session.getInstance(prop, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("hakaze.scarlet@gmail.com", "mpbsfybeskfvaspq");
-            }
-        });
+        SessionProvider sessionProvider = new SessionProvider();
+        Session session = sessionProvider.get();
 
         RecipientCsvParser recipientCsvParser = new RecipientCsvParser();
         List<Recipient> recipients = recipientCsvParser.parse();
@@ -49,7 +30,7 @@ public class Main {
             .collect(Collectors.joining(","));
 
         Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("hakaze.scarlet@gmail.com"));
+        message.setFrom(new InternetAddress(System.getenv("EMAIL")));
         message.setRecipients(
             Message.RecipientType.TO, InternetAddress.parse(emails));
         message.setSubject("1st app");
---
+++ src/main/java/SessionProvider.java
@@ -0,0 +1,32 @@
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import java.util.Properties;
+
+public class SessionProvider {
+
+    private final Properties properties;
+
+    public SessionProvider() {
+        properties = new Properties();
+        properties.put("mail.smtp.starttls", true);
+        properties.put("mail.smtp.starttls.enable", true);
+        properties.put("mail.smtp.starttls.required", true);
+        properties.put("mail.smtp.auth", true);
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", 587);
+        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        properties.put("mail.smtp.EnableSSL.enable", true);
+        properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+    }
+
+    public Session get() {
+        return Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(System.getenv("EMAIL"), System.getenv("TOKEN"));
+            }
+        });
+    }
+}
