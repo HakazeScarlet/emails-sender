78d3b3a25dce2550c03e73ce74db67079ab3dd72
nedelweiss <edelweissnull@gmail.com>
2023-07-31 18:54:57 +0300

    added TODO

--- src/main/java/session/SessionProvider.java
+++ src/main/java/session/SessionProvider.java
@@ -13,6 +13,8 @@ public abstract class SessionProvider {
         this.properties = properties;
     }
 
+    // TODO: identify when the session has expired and a new one needs to be created to fix exception below
+    // Caused by: javax.mail.AuthenticationFailedException: 454 4.7.0 Too many login attempts, please try again later. b8-20020a2e8488000000b002b6f1afd00esm2510417ljh.107 - gsmtp
     public Session get() {
         return Session.getInstance(properties, new Authenticator() {
             @Override
